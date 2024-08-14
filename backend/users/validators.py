81a059d80399a3c2a70f81c3e0802ced3b9f45a0
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-08-14 18:47:41 +0500

    add func for validate username

---
+++ backend/users/validators.py
@@ -0,0 +1,20 @@
+import re
+
+from django.core.exceptions import ValidationError
+
+
+def validate_username(value):
+    """Валидация для юзернейма."""
+
+    forbidden_values = ('me',)
+    if value.lower() in forbidden_values:
+        raise ValidationError(f'Недопустимое имя пользователя: {value}')
+
+    forbidden_chars = re.sub(r'^[\w.@+-]+\z', '', value)
+    if forbidden_chars:
+        raise ValidationError(
+            'Недопустимые символы в имени пользователя:'
+            .join(set(forbidden_chars))
+        )
+
+    return value
