4d30cdba28d173ab420a9527eb332029b8466ea9
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-08-24 16:20:28 +0500

    review_iteration#3: pep8 formatting

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -36,14 +36,12 @@ class RecipeFilter(FilterSet):
 
     def is_in_shopping_cart_filter(self, queryset, name, value):
         """Фильтрация для рецептов в списке покупок."""
-
         if value and self.request.user.is_authenticated:
-            return queryset.filter(shopping_carts__user=self.request.user)
+            return queryset.filter(shoppingcarts__user=self.request.user)
         return queryset
 
     def is_favorited_filter(self, queryset, name, value):
         """Фильтрация для рецептов в избранном."""
-
         if value and self.request.user.is_authenticated:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
