9312befce2c02cb39ff703161cf5a10e1f9dc8fd
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-08-27 15:30:10 +0500

    review_iteration#4: move func in api/

--- backend/api/download_shopping_cart.py
+++ backend/api/download_shopping_cart.py
@@ -1,7 +1,5 @@
 from datetime import date
-import tempfile
 
-from django.http import FileResponse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,10 +8,9 @@ load_dotenv()
 def download_txt(data, user):
     """Функция для загрузки списка покупок в txt формате."""
 
-    file_name = f'{user}`s_shopping_cart.txt'
     current_date = date.today().strftime('%d-%m-%Y')
-
     header = f'Список покупок {user}\nДата: {current_date}'
+
     file_data = '\n'.join([
         f'{item["ingredient__name"]} {item["total_amount"]} '
         f'{item["ingredient__measurement_unit"]}'
@@ -22,18 +19,4 @@ def download_txt(data, user):
 
     content = f'{header}\n\n{file_data}'
 
-    with tempfile.NamedTemporaryFile(
-        delete=False,
-        mode='w',
-        encoding='utf-8',
-    ) as temp_file:
-        temp_file.write(content)
-        temp_file_name = temp_file.name
-
-    response = FileResponse(
-        open(temp_file_name, 'rb'),
-        content_type='text/plain; charset=utf-8'
-    )
-    response['Content-Disposition'] = f'attachment; filename="{file_name}"'
-
-    return response
+    return content
