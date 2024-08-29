a978a9d0d92e65f709c347d4e64e0fb1209ffa79
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-08-29 12:49:36 +0500

    try to get api/data#3

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'foodgram_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'backend', 'docs')],
+        'DIRS': [os.path.join(BASE_DIR, 'docs')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
